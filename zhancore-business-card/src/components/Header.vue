828aa6763a429e77d82853491ac2e971a7862f63
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-09-14 17:55:59 +0500

    feat: add place for option toggles in Header.vue

--- zhancore-business-card/src/components/Header.vue
+++ zhancore-business-card/src/components/Header.vue
@@ -7,6 +7,8 @@ import Logo from "@/assets/logo.svg?component";
     <div class="container">
       <Logo class="header-logo" />
       <h2 class="header-text">ZHANCORE KZ</h2>
+      <div class="language-switcher option">Language switcher</div>
+      <div class="theme-toggle option">Light/Dark switcher</div>
     </div>
   </div>
 </template>
@@ -52,6 +54,23 @@ import Logo from "@/assets/logo.svg?component";
   position: absolute;
 
   left: -25px;
-  top: 13px;
+  top: 13px; /* 25 - 0.5 * (100 - header-text height) */
+}
+
+.option {
+  font-size: 24px;
+  width: 190px;
+  text-align: center;
+  color: #f5f5f5;
+}
+
+.language-switcher {
+  grid-column: 5;
+  justify-self: center;
+}
+
+.theme-toggle {
+  grid-column: 6;
+  justify-self: center;
 }
 </style>
