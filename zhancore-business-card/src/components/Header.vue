1884eb6371f57c872f80e16225458d0c84bb03ea
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-09-14 17:39:45 +0500

    fix: header layout

--- zhancore-business-card/src/components/Header.vue
+++ zhancore-business-card/src/components/Header.vue
@@ -3,46 +3,55 @@ import Logo from "@/assets/logo.svg?component";
 </script>
 
 <template>
-  <div class="main">
-    <div class="logo-box">
-      <Logo class="logo-icon" />
-      <span class="separator-line" />
-      <h2 class="logo-text">ZHANCORE KZ</h2>
+  <div class="wrapper">
+    <div class="container">
+      <Logo class="header-logo" />
+      <h2 class="header-text">ZHANCORE KZ</h2>
     </div>
   </div>
 </template>
 
 <style scoped>
-.main {
+.wrapper {
   background-color: #1e555c;
-  display: flex;
+  height: 100px;
+}
+
+.container {
   align-items: center;
   height: 100px;
 }
 
-.logo-box {
-  display: flex;
-  align-items: center;
-  gap: 25px;
-}
+.header-logo {
+  grid-column: 1;
+  justify-self: end;
 
-.logo-icon {
   fill: #21a0a0;
   width: 105px;
   height: 77px;
 }
 
-.separator-line {
-  display: block;
-  width: 3px;
-  height: 50px;
-  background-color: #eae0cc;
-}
+.header-text {
+  grid-column: 2;
+  justify-self: start;
+  position: relative;
 
-.logo-text {
   color: #f5f5f5;
   font-size: 32px;
   width: 190px;
-  height: auto;
+  height: 78px;
+  font-weight: bold;
+}
+
+.header-text::before {
+  display: block;
+  content: "";
+  width: 3px;
+  height: 50px;
+  background-color: #eae0cc;
+  position: absolute;
+
+  left: -25px;
+  top: 13px;
 }
 </style>
