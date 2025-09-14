994fd0f370e9b17f284fd6a6f0eb3a8bf11be393
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-09-14 22:04:04 +0500

    feat: add our clients and interested sections in Main.vue

--- zhancore-business-card/src/views/Main.vue
+++ zhancore-business-card/src/views/Main.vue
@@ -5,7 +5,6 @@ import LogoWithText from "@/assets/logo-with-text-colored.svg?component";
 
 <template>
   <TitleBlock class="title"> What are We? </TitleBlock>
-
   <div class="what-are-we container">
     <div class="image-section block">
       <LogoWithText class="logo-with-text" />
@@ -41,6 +40,44 @@ import LogoWithText from "@/assets/logo-with-text-colored.svg?component";
         By embracing the latest technologies and trends, we create products that stay
         relevant and competitive.
       </div>
+    </div>
+  </div>
+
+  <TitleBlock>Our Clients</TitleBlock>
+  <div class="our-clients container">
+    <div class="block client-review-block">
+      <img alt="Company Logo" />
+      <div class="client-review">Review</div>
+    </div>
+
+    <div class="block client-review-block">
+      <img alt="Company Logo" />
+      <div class="client-review">Review</div>
+    </div>
+
+    <div class="block client-review-block">
+      <img alt="Company Logo" />
+      <div class="client-review">Review</div>
+    </div>
+
+    <div class="block client-review-block">
+      <img alt="Company Logo" />
+      <div class="client-review">Review</div>
+    </div>
+  </div>
+
+  <TitleBlock>Interested?</TitleBlock>
+  <div class="interested container">
+    <div class="interested-wrapper">
+      <div class="label">Leave info about you</div>
+
+      <div class="inputs block">
+        <input type="text" id="fullname" placeholder="Full Name" />
+        <input type="text" id="contact-details" placeholder="Contact Details" />
+        <input type="text" id="ticket-reason" placeholder="Ticket Reason" />
+      </div>
+
+      <button id="submit">Submit</button>
     </div>
   </div>
 </template>
@@ -128,5 +165,97 @@ import LogoWithText from "@/assets/logo-with-text-colored.svg?component";
 
 .description-section > .block {
   height: 177px;
+}
+
+.our-clients {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 50px;
+}
+
+.our-clients > .block {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.client-review-block > img {
+  flex-shrink: 0;
+  height: 190px;
+  width: 190px;
+  background-color: rgb(255, 255, 255);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  color: #000000;
+  text-align: center;
+}
+
+.client-review-block > .client-review {
+  flex: 1;
+  text-align: left;
+  display: flex;
+  align-items: center;
+}
+
+.interested-wrapper {
+  grid-column: 1 / -1;
+  display: flex;
+  width: 100%;
+}
+
+.interested-wrapper > .label {
+  flex: 430;
+
+  background-color: #21a0a0;
+  height: 342px;
+  color: #fafafa;
+  font-weight: 600;
+  font-size: 40px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.interested-wrapper > .inputs {
+  flex: 800;
+
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  gap: 12px;
+}
+
+.inputs > input {
+  border-radius: 0;
+  width: 100%;
+  height: 66px;
+  border: none;
+  font-size: 32px;
+  padding: 0 15px;
+}
+
+.inputs > input::placeholder {
+  color: #8a8a8a;
+  opacity: 0.5;
+}
+
+#ticket-reason {
+  width: 80%;
+}
+
+.interested-wrapper > #submit {
+  flex: 190;
+
+  font-size: 32px;
+  background-color: #1c8e8e;
+  border: none;
+  color: #fafafa;
 }
 </style>
