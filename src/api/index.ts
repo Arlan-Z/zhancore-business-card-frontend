2a1cec3a61400cf13b9b19bffb956c332fe4221f
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-12-19 04:02:49 +0500

    fix: remove CORS error

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,20 @@
-import type Project from './models/project'
+import api from './api';
 
 export const getProjects = async () => {
-  const response = await fetch('/api/project-reviews');
-  console.log(response)
-  if (!response.ok) throw new Error('Failed to fetch projects');
-  return response.json();
+  const { data } = await api.get('/project-reviews');
+  return data;
 };
 
 export const getClients = async () => {
-  const response = await fetch('/api/client-reviews');
-  if (!response.ok) throw new Error('Failed to fetch clients');
-  return response.json();
+  const { data } = await api.get('/client-reviews');
+  return data;
 };
 
-export const saveContact = async (contact: { name: string; details: string; message: string }) => {
-  const response = await fetch ('/api/client/landing-request', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(contact)
-  });
-  if (!response.ok) throw new Error('Failed to save contact');
-  return response.json();
+export const saveContact = async (contact: {
+  name: string;
+  details: string;
+  message: string;
+}) => {
+  const { data } = await api.post('/api/client/landing-request', contact);
+  return data;
 };
